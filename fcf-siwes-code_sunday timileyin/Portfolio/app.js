db580eb3a0a6216037a39a015c56e70875f4badd
Timi123-Andy <andynate163@gmail.com>
2024-11-22 03:57:41 -0800

    Add files via upload

---
+++ fcf-siwes-code_sunday timileyin/Portfolio/app.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('mobile-menu');
+const menuLink = document.querySelector('.navbar_manu');
+
+manu.addEventListener('click', function() {
+    manu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active');
+}
+)
---
+++ fcf-siwes-code_sunday timileyin/pspractice.js
@@ -0,0 +1,10 @@
+ function myProject() {
+    document.getElementById("demo").innerHTML = "Content is now changed! Congratulations!.";
+}
+/* document.getElementById("demo").innerHTML = 5 + 6;
+
+ document.write(5 + 6);
+
+ alert(5 + 6)
+
+ console.log
